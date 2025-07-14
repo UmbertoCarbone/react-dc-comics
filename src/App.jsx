be5f1fc301dcc9f9a0993f5856b4880909c13119
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-07-14 18:06:27 +0200

    Aggiungi componente Main e stili CSS per layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css' */
+/* import "./index.css" */ 
 import Header from './assets/components/Header'
-import Header from './assets/components/Main'
+import Main from "./assets/components/Main"
 
 function App() {
-  
+
 
   return (
     <>
-<Header />
-{/* <Main />
-<Footer /> */}
+      <Header />
+      <Main />
     </>
   )
 }
---
+++ src/assets/components/main.jsx
@@ -0,0 +1,23 @@
+import foto1 from '/img/buy-comics-digital-comics.png';
+import foto2 from "/img/buy-comics-merchandise.png"
+import foto3 from "/img/buy-comics-subscriptions.png"
+import foto4 from "/img/buy-comics-shop-locator.png"
+import foto5 from "/img/buy-dc-power-visa.svg"
+
+
+
+export default function Main() {
+    return (
+        <main>
+            <div className="divblack"><h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum saepe, neque dolore ex architecto nobis.</h1></div>
+            <div className="divlightblue d-flex justify-content-center align-items-center">
+                <a href="#"><img src={foto1} alt="Digital Comics" />DIGITAL COMICS</a>
+                <a href="#"><img src={foto2} alt="DC Merchandise" />DIGITAL COMICS</a>
+                <a href="#"> <img src={foto3} alt="Subscription" />DIGITAL COMICS</a>
+                <a href="#"> <img src={foto4} alt="Shop Locator" />DIGITAL COMICS</a>
+                <a href="#"> <img src={foto5} alt="Power Visa" />DIGITAL COMICS</a>
+            </div>
+        </main>
+    )
+}
+
