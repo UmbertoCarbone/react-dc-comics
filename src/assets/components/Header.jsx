4b50e2afd6bc3f3c0bd6b869ac9d67c99d7fbe85
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-07-15 13:38:59 +0200

    navbar con jsx

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,24 +1,82 @@
 //import photo
 import dclogo from '/img/dc-logo.png';
 
+const menu = [
+    {
+        id: 1,
+        href: "#",
+        title: "Characters",
 
-//expor function
+    },
+    {
+        id: 2,
+        href: "#",
+        title: "Comics",
+
+    },
+    {
+        id: 3,
+        href: "#",
+        title: "Movies",
+
+    },
+    {
+        id: 4,
+        href: "#",
+        title: "TV",
+
+    },
+    {
+        id: 5,
+        href: "#",
+        title: "Games",
+
+    },
+    {
+        id: 6,
+        href: "#",
+        title: "Collectibles",
+
+    },
+    {
+        id: 7,
+        href: "#",
+        title: "Videos",
+
+    },
+    {
+        id: 8,
+        href: "#",
+        title: "Fans",
+
+    },
+    {
+        id: 9,
+        href: "#",
+        title: "News",
+
+    },
+    {
+        id: 10,
+        href: "#",
+        title: "Shop",
+
+    }
+];
+
+
 export default function Header() {
     return (
         <header>
-
             <nav className="navbar">
                 <img src={dclogo} alt="DC Logo" />
-                <a href="#">CHARACTERS</a>
-                <a href="#">COMICS</a>
-                <a href="#">MOVIES</a>
-                <a href="#">TV</a>
-                <a href="#">GAMES</a>
-                <a href="#">COLLECTIBLES</a>
-                <a href="#">VIDEOS</a>
-                <a href="#">FANS</a>
-                <a href="#">NEWS</a>
-                <a href="#">SHOP</a>
+                {
+                    menu.map((item) => {
+                        return (
+                            <a classname="" href="#">{item.title}</a>
+                        )
+                    })
+                }
             </nav>
         </header>
     )
